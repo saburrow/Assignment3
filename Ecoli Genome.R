ac73d730aa1864addd13bda89a5b7c77b3332585
saburrow <saburrow@deakin.edu.au>
2020-06-12 21:09:45 +1000

    Question 4 answered. Continue with 5. Q4. has been completed accordingly.

--- Ecoli Genome.R
+++ Ecoli Genome.R
@@ -8,51 +8,35 @@ library("Biostrings")
 
 #1. Download the whole set of E.coli gene DNA sequences and use gunzip to decompress. Use the makeblast() function to create a blast database.
 #How many sequences are present in the E.coli set?
-download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz", destfile = "Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa")
-R.utils::gunzip ("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa", overwrite=TRUE)
+download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz", destfile = "ECOLI")
+R.utils::gunzip ("ECOLI", overwrite=TRUE)
 
-makeblastdb("https://raw.githubusercontent.com/saburrow/Assignment3/master/Escherichia_coliz",dbtype = "nucl")
+
+makeblastdb("ECOLI" ,dbtype = "nucl") 
+
 
 #2.Download the sample fasta sequence and read them in as above. For allocated sequence, determine the length (in bp) and the proportion of GC bases?
 download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/sample.fa", destfile = "student41")
 R.utils::gunzip("student41")
-Student41seq<-read.fasta("student41")
-
+seq<-read.fasta("student41")
+Student41seq<-seq[[41]]
 str(Student41seq)
 
-seq41.Ecoli<-(Student41seq$`41`)
-head(seq41.Ecoli)
+seq41.Ecoli<-(Student41seq)
+str(seq41.Ecoli)
 seqinr::getLength(seq41.Ecoli)
-seqinr::GC(Student41seq$`41`)
 
-read.csv("Escherichia_coli2.gz")
+seqinr::GC(seq41.Ecoli)
 
 #3. Use Blast to identify what E.coli gene your sequence matches. Show a table of the top 3 hits including percent identity. E-value and bit scores.
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/mutblast_functions.R", destfile = "Mutblast.R")
+source("Mutblast.R")
+res<-myblastn_tab(seq41.Ecoli,db="ECOLI") 
+res
+View(res)
 
-function(myseq= seq41.Ecoli, db="ECOLI")
- {
-mytmpfile1 <- tempfile(seq41.Ecoli)
-mytmpfile2 <- tempfile("Escherichia_coli2.gz")
-
-write.fasta(myseq, names = attr(myseq, "sequence41"), file.out = mytmpfile1)
-
-system2(command = "/usr/bin/blastn", args = paste("-db ", 
-      db, " -query", mytmpfile1, "-outfmt 6 -evalue 0.05 -ungapped >", 
-       mytmpfile2))
-    res <- NULL
-if(file.info(mytmpfile2)$size>0) {
-      res <- read.csv(mytmpfile2, sep = "\t", header = FALSE)
-        colnames(res) <- c("qseqid", "sseqid", "pident", "length", 
-"mismatch", "gapopen", "qstart", "qend", "sstart", 
-           "send", "evalue", "bitscore")
-}
-unlink(c(mytmpfile1, mytmpfile2))
-   if (!is.null(res)) {
-res <- res[order(-res$bitscore), ]
-   }
-res}
-
-head
-
-
-
+#4. Run the function and write an R code to check the number of mismatches between the original and mutated sequence?
+mySeqmut<-mutator(Student41seq,nmut=100)
+mySeqmut
+res2<-myblastn_tab(mySeqmut,db=) 
+View(res2)
